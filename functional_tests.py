466c62f1d731c9fef3c3783caad9b57602784be8
Ikenna Anosike <anosike212@gmail.com>
2021-08-17 13:51:03 +0100

    use unittest in FT

---
+++ functional_tests.py
@@ -0,0 +1,37 @@
+from selenium import webdriver
+import unittest
+
+
+class NewVisitorTest(unittest.TestCase):
+    
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_make_a_text_post(self):
+        # Alice checks out the homepage to the hottest social network site
+        self.browser.get("http://localhost:8000")
+
+        # She notices the sleek page title contains the site name
+        self.assertIn("Connect Social Network", self.browser.title)
+
+        # she sees a textbox that invites her to 'write something'
+
+        # she types 'Hello everybody' into the textbox
+
+        # when she hits post, the page updates and now contains
+        # 'Hello everybody' and the timestamp
+
+        # There is still a textbox inviting her to add another item.
+        # She enters 'Guess who's back?'
+
+        # The page updates, and now shows both items
+
+        # satisfied, she closes her browser
+        self.browser.quit()
+
+
+if __name__ == "__main__":
+    unittest.main()
